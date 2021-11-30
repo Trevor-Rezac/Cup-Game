df6e9b0cbad27beb31653b87ff0d3bb5eac9d908
TrevorRezac <trev.rezac@gmail.com>
2021-11-29 20:20:39 -0800

    created cup game with reset button

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-## The Golden Rule: 
+## HTML Setup (what elements do we need?)
+1. 3 divs for cup images, inside one img container
+    - Why?
+    We need to show the user which cup the ball was hidden under.
+    - How?
+    Update the dom image element (img.src)
+2. 3 button elements
+    - Why?
+    Tells us which cup the user picks, and when they pick it.
+    - How?
+    addEventListener to the buttons
+3. 3 divs for Win/Loss/Total, inside a separate container
+    - Why? 
+    To display the current "state", 
+    - How? 
+    .textContent to update the corresponding <span> element
 
-🦸 🦸‍♂️ `Stop starting and start finishing.` 🏁
+## Events
+1. user clicks any of the 3 buttons
+    - (reset image of all cups)
+    
+    ..set state..
+    - increment total attempts
+    - use math.random() to decide which cup is correct
+    - we need to compare the correct cup with the cup the user clicked
+        if the user clicked the correct cup, increment win
+        if the user did NOT click the click correct cup, increment loss (because losses can be derived by subtracting wins from total attempts, WE SHOULD NOT TRACK as state)
 
-If you work on more than one feature at a time, you are guaranteed to multiply your bugs and your anxiety.
-
-## Making a plan
-
-1) **Make a drawing of your app. Simple "wireframes"**
-1) **Once you have a drawing, name the HTML elements you'll need to realize your vision**
-1) **For each HTML element ask: Why do I need this?** 
-1) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How"**
-1) **Find all the 'events' (user clicks, form submit, on load etc) in your app. Ask one by one, "What happens when" for each of these events. Does any state change?**
-1) **Think about how to validate each of your features according to a Definition of Done**
-1) **Consider what features _depend_ on what other features. Use this dependency logic to figure out what order to complete tasks.**
-
-Additional considerations:
-- Ask: which of your HTML elements need to be hard coded, and which need to be dynamically generated?
-- Consider your data model. 
-  - What kinds of objects (i.e., Dogs, Friends, Todos, etc) will you need? 
-  - What are the key/value pairs? 
-  - What arrays might you need? 
-  - What needs to live in a persistence layer?
-- Is there some state we need to initialize?
-- Ask: should any of this work be abstracted into functions? (i.e., is the work complicated? can it be resused?)
+    ..update DOM..
+    - change image of correct cup
+    - change number of Wins/loss/Attempts
+    
+2. **stretch** user clicks 'try again' button
+    - (reset image of all cups)
--- app.js
+++ app.js
@@ -1,8 +1,105 @@
 // import functions and grab DOM elements
+const cupA = document.getElementById('cup-A-img');
+// console.log(cupA);
+const cupB = document.getElementById('cup-B-img');
+const cupC = document.getElementById('cup-C-img');
+
+const cupABtn = document.getElementById('cup-A-btn');
+// console.log(cupABtn);
+const cupBBtn = document.getElementById('cup-B-btn');
+const cupCBtn = document.getElementById('cup-C-btn');
+
+const winsEl = document.getElementById('wins');
+// console.log(winEl);
+const lossesEl = document.getElementById('losses');
+const attemptsEl = document.getElementById('attempts');
+
+const resetBtn = document.getElementById('reset-btn');
 
 // let state
+let wins = 0;
+let attempts = 0;
+
+
+function resetImgs() {
+  // - (reset image of all cups)
+    cupA.src = './assets/original-cup.png';
+    cupB.src = './assets/original-cup.png';
+    cupC.src = './assets/original-cup.png';
+}
+
+console.log(resetImgs());
+
+function updateWinLossAtt() {
+    winsEl.textContent = wins;
+    attemptsEl.textContent = attempts;
+    lossesEl.textContent = attempts - wins;
+}
 
 // set event listeners 
+cupABtn.addEventListener('click', () => {
+  // - (reset image of all cups)
+    resetImgs();
+    console.log(resetImgs());
+  //   ..set state..
+  //   - increment total attempts
+    attempts++;
+  //   - use math.random() to decide which cup is correct
+    const correctCup = Math.floor(Math.random() * 3);
+    // console.log(correctCup);
+
+  //   - we need to compare the correct cup with the cup the user clicked
+  //        if the user clicked the correct cup, increment win
+    if (correctCup === 0) {
+        cupA.src = './assets/correct-cup.png';
+        wins++;
+        console.log(cupA.src);
+//       if the user did NOT click the click correct cup, increment loss (because losses can be derived by subtracting wins from total attempts, WE SHOULD NOT TRACK as state)
+    } else {
+        cupA.src = './assets/wrong-cup.png';
+    }
+  //   ..update DOM..
+  //   - change image of correct cup
+  //   - change number of Wins/loss/Attempts
+    updateWinLossAtt();
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+cupBBtn.addEventListener('click', () => {
+    resetImgs();
+    attempts++;
+
+    const correctCup = Math.floor(Math.random() * 3);
+
+    if (correctCup === 1) {
+        cupB.src = './assets/correct-cup.png';
+        wins++;
+    } else {
+        cupB.src = './assets/wrong-cup.png'; 
+    }
+
+    updateWinLossAtt();
+});
+
+cupCBtn.addEventListener('click', () => {
+    resetImgs();
+    attempts++;
+
+    const correctCup = Math.floor(Math.random() * 3);
+
+    if (correctCup === 2) {
+        cupC.src = './assets/correct-cup.png';
+        wins++;
+    } else {
+        cupC.src = './assets/wrong-cup.png'; 
+    }
+
+    updateWinLossAtt();
+});
+
+resetBtn.addEventListener('click', () => {
+    resetImgs();
+});
